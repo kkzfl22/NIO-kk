5bce0be5a83f7d79a9f9f7b57e7dca90c54597b0
liujun <546064298@qq.com>
2017-04-15 23:32:56 +0800

    添加文档，及连接的状态类信息

---
+++ src/main/java/com/kk/nio/mysql/console/MysqlConnStateEnum.java
@@ -0,0 +1,39 @@
+package com.kk.nio.mysql.console;
+
+import com.kk.nio.mysql.connection.MysqlConnStateInf;
+import com.kk.nio.mysql.connection.state.MysqlConnCreateState;
+
+/**
+ * mysql的连接的状态信息
+ * 
+ * @since 2017年4月14日 下午4:12:45
+ * @version 0.0.1
+ * @author liujun
+ */
+public enum MysqlConnStateEnum {
+
+	/**
+	 * 创建连接状态
+	 */
+	MYSQL_CONN_STATE_CREATE(new MysqlConnCreateState())
+
+	;
+
+	/**
+	 * 状态信息
+	 */
+	private MysqlConnStateInf connState;
+
+	private MysqlConnStateEnum(MysqlConnStateInf connState) {
+		this.connState = connState;
+	}
+
+	public MysqlConnStateInf getConnState() {
+		return connState;
+	}
+
+	public void setConnState(MysqlConnStateInf connState) {
+		this.connState = connState;
+	}
+
+}
